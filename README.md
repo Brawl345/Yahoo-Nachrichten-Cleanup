60eeb9b27568ed892cde253be8064a1c894819d1
Andreas Bielawski <brawl98@outlook.com>
2016-11-09 16:42:14 +0100

    - "Yahoo Stars Deutschland" hinzugefügt - Changelog zur README hinzugefügt

--- README.md
+++ README.md
@@ -8,3 +8,22 @@ Installiere einen Script-Manager deiner Wahl:
 * Für Firefox: [Greasemonkey](https://addons.mozilla.org/de/firefox/addon/greasemonkey/)
 
 Danach einfach das [Userscript](https://raw.githubusercontent.com/Brawl345/Yahoo-Nachrichten-Cleanup/master/cleanup.user.js) installieren.
+
+## Changelog
+### v1.0.3
+* "Yahoo Stars Deutschland" hinzugefügt
+
+### v1.0.2
+* "yahoo-nachrichten.de" hinzugefügt
+* "BLZ" hinzugefügt
+* "KSTA" hinzugefügt
+
+### v1.0.1
+* "Business Insider" zu "Business Insider DE" korrigiert
+* "Euronews Videos" hinzugefügt
+* "WENN" hinzugefügt
+* "Yahoo Stars" hinzugefügt
+* "Zoomin.tv" hinzugefügt
+
+### v1.0
+* Erste Veröffentlichung
--- cleanup.user.js
+++ cleanup.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name       Yahoo Nachrichten Cleanup
-// @version    1.0.2
+// @version    1.0.3
 // @namespace  https://github.com/Brawl345/Yahoo-Nachrichten-Cleanup
 // @author	   iCON, Edward (orig)
 // @description  Entfernt nervige Nachrichtenquellen von Yahoo! Nachrichten
@@ -14,7 +14,7 @@
 setInterval(removeSpam, 2000);
 function removeSpam() {
 var spanTags = document.getElementsByTagName('span');
-var spamNames = ['Abendzeitung', 'Business Insider DE', 'Yahoo Nachrichten', 'Yahoo Video, Bitprojects', 'Yahoo, spot on news', 'Yahoo', 'Cover Media Video', 'Euronews Videos', 'WENN', 'Yahoo Stars', 'Zoomin.tv', 'yahoo-nachrichten-de', 'BLZ', 'KSTA'];
+var spamNames = ['Abendzeitung', 'Business Insider DE', 'Yahoo Nachrichten', 'Yahoo Video, Bitprojects', 'Yahoo, spot on news', 'Yahoo', 'Cover Media Video', 'Euronews Videos', 'WENN', 'Yahoo Stars', 'Zoomin.tv', 'yahoo-nachrichten-de', 'BLZ', 'KSTA', 'Yahoo Stars Deutschland'];
 var found;
 for (var i = 0; i < spanTags.length; i++) {
   if (contains(spamNames, spanTags[i].textContent)) {
